5101486d296c742e1e8b4d8e77fd328bdbf64956
Liu <1444611@fcps.edu>
2020-02-05 15:58:41 -0500

    Initial commit

---
+++ app/src/main/java/com/liusara/quizapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.liusara.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button submitButton;
+    EditText responseText;
+    TextView displayText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        submitButton=findViewById(R.id.clickButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Hello mom");
+                Log.i("testButton", "Hi dad!");
+            }
+        });
+    }
+}
