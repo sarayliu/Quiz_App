7d5e4355494c45e6f3d1db433a222e56ba513022
1444611 <sarayaoliu@gmail.com>
2020-02-10 16:00:48 -0500

    2/10/20

--- app/src/main/java/com/liusara/quizapp/MainActivity.java
+++ app/src/main/java/com/liusara/quizapp/MainActivity.java
@@ -18,11 +18,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         submitButton=findViewById(R.id.clickButton);
+        responseText=findViewById(R.id.responseEditText);
+        displayText=findViewById(R.id.textBox);
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 System.out.println("Hello mom");
-                Log.i("testButton", "Hi dad!");
+                Log.i("testButton", "Hi dad! "+responseText.getText());
+                String concatenatedString = displayText.getText()+" "+responseText.getText();
+                displayText.setText(concatenatedString);
+            }
+        });
+        responseText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus){
+                    if(responseText.getText().toString().equals("TJ")){
+                        displayText.setText("TJ Rocks!");
+                        responseText.setText("");
+                        responseText.setHint("That's a good name.");
+                    }
+                }
             }
         });
     }
